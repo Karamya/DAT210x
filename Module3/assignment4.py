94e73d477cbe91c9f97153bce9902ca7d01f4beb
Karamya <Karamya@users.noreply.github.com>
2016-09-20 16:11:13 +0200

    Update assignment4.py

--- Module3/assignment4.py
+++ Module3/assignment4.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
-
+import os
 from pandas.tools.plotting import parallel_coordinates
 
 # Look pretty...
 matplotlib.style.use('ggplot')
-
-
+os.chdir('D:/Data analysis/data/DAT210x/Module3/Datasets')
 #
 # TODO: Load up the Seeds Dataset into a Dataframe
 # It's located at 'Datasets/wheat.data'
 # 
 # .. your code here ..
+wheat_data = pd.read_csv('wheat.data', header = 0)
+#print(wheat_data)
 
 
 
@@ -21,15 +22,16 @@ matplotlib.style.use('ggplot')
 # 
 # .. your code here ..
 
-
-
+wheat_data.drop(labels = ['id', 'area', 'perimeter'], axis = 1, inplace = True)
+print(wheat_data)
 #
 # TODO: Plot a parallel coordinates chart grouped by
 # the 'wheat_type' feature. Be sure to set the optional
 # display parameter alpha to 0.4
 # 
 # .. your code here ..
-
+plt.figure()
+parallel_coordinates(wheat_data, 'wheat_type', alpha = 0.4 )
 
 
 plt.show()
