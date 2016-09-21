54d5295933c4394d97f2dc68c6d7084321b3fd30
Karamya <Karamya@users.noreply.github.com>
2016-09-21 15:58:45 +0200

    Isomap 2D and 3D face dataset
    loaded a .mat file

--- Module4/assignment4.py
+++ Module4/assignment4.py
@@ -6,6 +6,9 @@ import random, math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
+from sklearn.decomposition import PCA
+from sklearn.manifold import Isomap
+
 def Plot2D(T, title, x, y, num_to_plot=40):
   # This method picks a bunch of random samples (images in your case)
   # to plot onto the chart:
@@ -52,7 +55,10 @@ for i in range(num_images):
 # y is the principal component you want displayed on the y-axis, Can be 1 or 2
 #
 # .. your code here ..
-
+pca = PCA(n_components = 3)
+pca.fit(df)
+T = pca.transform(df)
+Plot2D(T, 'PCA Transformed Data', 1, 2, num_to_plot = 40)
 
 #
 # TODO: Implement Isomap here. Reduce the dataframe df down
@@ -60,13 +66,38 @@ for i in range(num_images):
 # the first two components.
 #
 # .. your code here ..
-
+iso = Isomap(n_neighbors = 8, n_components = 3)
+iso.fit(df)
+isotrans = iso.transform(df)
+Plot2D(isotrans, 'Isomap Transformed Data', 1, 2, num_to_plot = 40)
 
 #
 # TODO: If you're up for a challenge, draw your dataframes in 3D
 # Even if you're not, just do it anyway.
 #
 # .. your code here ..
+def Plot3D(T, title, x, y, z, num_to_plot=40):
+  # This method picks a bunch of random samples (images in your case)
+  # to plot onto the chart:
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection = '3d')
+  ax.set_title(title)
+  ax.set_xlabel('Component: {0}'.format(x))
+  ax.set_ylabel('Component: {0}'.format(y))
+  ax.set_zlabel('Component: {0}'.format(z))
+  x_size = (max(T[:,x]) - min(T[:,x])) * 0.08
+  y_size = (max(T[:,y]) - min(T[:,y])) * 0.08
+  z_size = (max(T[:,z]) - min(T[:,z])) * 0.08
+  for i in range(num_to_plot):
+    img_num = int(random.random() * num_images)
+    x0, y0, z0 = T[img_num,x]-x_size/2., T[img_num,y]-y_size/2., T[img_num,z]-z_size/2.
+    x1, y1, z1 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2., T[img_num,z]+z_size/2.
+    x2, y2, z2 = T[img_num,x]+x_size/2., T[img_num,y]+y_size/2., T[img_num,z]+z_size/2.
+    img = df.iloc[img_num,:].reshape(num_pixels, num_pixels)
+    ax.imshow(img, aspect='auto', cmap=plt.cm.gray, interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
-
+  # It also plots the full scatter:
+  ax.scatter(T[:,x],T[:,y], T[:,z], marker='.',alpha=0.7)
+Plot3D(T, "PCA Transformed into 3D", 0, 1, 2, num_to_plot = 40)
+Plot3D(isotrans, "Isomap Transformed into 3D", 0, 1, 2, num_to_plot = 40)
 plt.show()
