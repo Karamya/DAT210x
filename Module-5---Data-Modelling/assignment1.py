197a9ae07c580b1a10088965351c2650d5ec1444
Karamya <Karamya@users.noreply.github.com>
2016-09-22 14:34:08 +0200

    K- Means
    Gambling crime reports at Chicago

--- Module-5---Data-Modelling/assignment1.py
+++ Module-5---Data-Modelling/assignment1.py
@@ -2,10 +2,11 @@
 # TOOD: Import whatever needs to be imported to make this work
 #
 # .. your code here ..
-
-
+import pandas as pd
+from matplotlib import pyplot as plt
+import os
 matplotlib.style.use('ggplot') # Look Pretty
-
+from sklearn.cluster import KMeans
 
 #
 # TODO: To procure the dataset, follow these steps:
@@ -15,33 +16,38 @@ matplotlib.style.use('ggplot') # Look Pretty
 #    show up, since it has to load the entire list first.
 # 3. Scroll down to 'GAMBLING'
 # 4. Click the light blue 'Export' button next to the 'Filter' button, and select 'Download As CSV'
+os.chdir('D:/Data analysis/data/DAT210x/Module5/Datasets')
 
-
+#print(df)
 
 def doKMeans(df):
   #
   # INFO: Plot your data with a '.' marker, with 0.3 alpha at the Longitude,
   # and Latitude locations in your dataset. Longitude = x, Latitude = y
-  fig = plt.figure()
-  ax = fig.add_subplot(111)
-  ax.scatter(df.Longitude, df.Latitude, marker='.', alpha=0.3)
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(df.Longitude, df.Latitude, marker='.', alpha=0.3)
 
   #
   # TODO: Filter df so that you're only looking at Longitude and Latitude,
   # since the remaining columns aren't really applicable for this purpose.
   #
   # .. your code here ..
-
+    #print(df.columns)
+    df = df[['Longitude', 'Latitude']]    
+    #print(df)
   #
   # TODO: Use K-Means to try and find seven cluster centers in this df.
   #
   # .. your code here ..
-
+    kmeans_model = KMeans(n_clusters = 7)    
+    kmeans_model.fit(df)
+    kmeans_model.predict(df)
   #
   # INFO: Print and plot the centroids...
-  centroids = kmeans_model.cluster_centers_
-  ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
-  print centroids
+    centroids = kmeans_model.cluster_centers_
+    ax.scatter(centroids[:,0], centroids[:,1], marker='x', c='red', alpha=0.5, linewidths=3, s=169)
+    print (centroids)
 
 
 
@@ -49,30 +55,33 @@ def doKMeans(df):
 # TODO: Load your dataset after importing Pandas
 #
 # .. your code here ..
-
+df = pd.read_csv('Crimes_-_2001_to_present.csv')
 
 #
 # TODO: Drop any ROWs with nans in them
 #
 # .. your code here ..
-
+df.dropna(axis = 0, inplace = True)
 
 #
 # TODO: Print out the dtypes of your dset
 #
 # .. your code here ..
-
+print(df.dtypes)
 
 #
 # Coerce the 'Date' feature (which is currently a string object) into real date,
 # and confirm by re-printing the dtypes. NOTE: This is a slow process...
 #
 # .. your code here ..
-
-
+df.Date = pd.to_datetime(df.Date, errors = 'coerce')
+print(df.dtypes)
 # INFO: Print & Plot your data
-doKMeans(df)
+y = df.Date.min()
 
+
+doKMeans(df)
+plt.title('Gambling crimes reported since ' + str(y)[:-8])
 
 #
 # TODO: Filter out the data so that it only contains samples that have
@@ -80,11 +89,12 @@ doKMeans(df)
 # crime incidents, as well as a new K-Means run's centroids.
 #
 # .. your code here ..
-
+df = df[df.Date > '2011-01-01']
 
 
 # INFO: Print & Plot your data
 doKMeans(df)
+plt.title('Gambling crimes reported after 01-01-2011 ')
 plt.show()
 
 
